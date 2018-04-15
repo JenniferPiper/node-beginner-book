4fd3acde0dc2e408a0f365874674f21fe330647b
Jennifer Williams <jennifer@squareandplum.com>
2018-04-14 17:24:03 -0700

    handle blocking requests asynchronously

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,29 @@
-function start() {
-  console.log("Request handler 'start' was called.");
-  return "Hello Start";
+var exec = require('child_process').exec;
+
+function start(response) {
+  console.log('Request handler "start" was called.');
+
+  exec('ls -lah', function (error, stdout, stderr) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(stdout);
+    response.end();
+  });
+
+
+  // function sleep(milliseconds) {
+  //   var startTime = new Date().getTime();
+  //   while (new Date().getTime() < startTime + milliseconds);
+  // }
+
+  //sleep(10000);
+ // return "Hello Start";
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello Upload');
+  response.end();  
 }
 
 exports.start = start;
