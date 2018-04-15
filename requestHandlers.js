e5f1619a0607ac8f3e8248b6c65c88971e5ca537
Jennifer Williams <jennifer@squareandplum.com>
2018-04-14 17:48:36 -0700

    add form to upload user-input text

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,29 +1,31 @@
-var exec = require('child_process').exec;
-
-function start(response) {
+var querystring = require('querystring');
+function start(response, postData) {
   console.log('Request handler "start" was called.');
 
-  exec('ls -lah', function (error, stdout, stderr) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(stdout);
-    response.end();
-  });
+  var body = '<html>' +
+ '<head>'+
+ '<meta http-equiv="Content-Type" content="text/html; ' +
+ 'charset=UTF-8" />' +
+ '</head>' +
+ '<body>' +
+ '<form action="/upload" method="post">' +
+ '<textarea name="text" rows="20" cols="60"></textarea>' +
+  '<input type="submit" value="Submit some text" />' +
+ '</form>' +
+'</body>' +
+ '</html>';
 
-
-  // function sleep(milliseconds) {
-  //   var startTime = new Date().getTime();
-  //   while (new Date().getTime() < startTime + milliseconds);
-  // }
-
-  //sleep(10000);
- // return "Hello Start";
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(body);
+  response.end();
 }
 
-function upload(response) {
-  console.log("Request handler 'upload' was called.");
+function upload(response, postData) {
+  console.log('Request handler \'upload\' was called.');
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello Upload');
-  response.end();  
+  response.write('You\'ve sent:\n' +
+  querystring.parse(postData).text);
+  response.end();
 }
 
 exports.start = start;
